0bba616c6dd7ee9a61d5836ffda233869f4805b6
SJ.C <12246544+Noctilucos@users.noreply.github.com>
2020-12-28 10:01:44 +0000

    fix

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -9,28 +9,28 @@ $(document).ready(function(){
     // carousel
     var carouselWidth = document.querySelector('.carousel-wrapper').getClientRects()[0].width
     var carouselControl = 0
+
     $('.prev-icon').on('click', function(e){
+        carouselControl -= carouselWidth - 10
         if(carouselControl <= 0) {
             carouselControl = 0
             $('.carousel-wrapper ul').css('transform', 'translateX(-'+ carouselControl +'px)')
             $('.prev-icon').addClass('disabled')
         } else {
-            carouselControl -= carouselWidth - 10
             $('.carousel-wrapper ul').css('transform', 'translateX(-'+ carouselControl +'px)')
             $('.next-icon').removeClass('disabled')
         }
     })
 
     $('.next-icon').on('click', function(e){
-        if(carouselControl >= 3 * carouselWidth - 40) {
-            carouselControl = 3 * carouselWidth - 40
+        carouselControl += carouselWidth - 10
+        if(carouselControl >= 2 * carouselWidth - 40) {
+            carouselControl = 2 * carouselWidth - 40
             $('.carousel-wrapper ul').css('transform', 'translateX(-'+ carouselControl +'px)')
             $('.next-icon').addClass('disabled')
         } else {
-            carouselControl += carouselWidth - 10
             $('.carousel-wrapper ul').css('transform', 'translateX(-'+ carouselControl +'px)')
             $('.prev-icon').removeClass('disabled')
-
         }
     })
 
