5a20c1798036afac1b8eecaa5e60227917a00c58
SJ.C <12246544+Noctilucos@users.noreply.github.com>
2021-01-13 01:34:07 +0000

    update

---
+++ assets/js/footer.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    $('#footerSubscribe').keydown(function (e) {
+        if (e.keyCode === 13) {
+            subscribe()
+
+        }
+    })
+    $('#btnFooterSubscribe').click(function () {
+        subscribe()
+    })
+    $('#toTop').click(function () {
+        window.scrollTo(0, 0)
+    })
+    $(window).scroll(function () {
+        if (window.scrollY > 0) {
+            $('#toTop').show()
+        } else {
+            $('#toTop').hide()
+        }
+    })
+})
+
+function subscribe() {
+    $.ajax({
+        type: 'POST',
+        url: WebRoot + '/SubscribeNewsletter.ashx',
+        data: {
+            Email: $('#footerSubscribe').val()
+        },
+        success: function (res) {
+            alert("Subscribed. Thank you!");
+        }
+    })
+}
